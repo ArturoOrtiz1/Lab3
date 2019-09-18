0cbed23e565a1228245272824b9506f38853bde4
ArturoOrtiz1 <54646804+ArturoOrtiz1@users.noreply.github.com>
2019-09-17 22:03:59 -0400

    Add files via upload

---
+++ Class4.py
@@ -0,0 +1,22 @@
+from graphics import *
+def main():
+        win=GraphWin()
+        shape=Rectangle(Point(10,30),Point(40,70))
+        shape.setOutline("red")
+        shape.setFill("red")
+        shape.draw(win)
+        for i in range (10):
+            p=win.getMouse()
+            cornerPoint=shape.getP1()
+            c=shape.getCenter()
+            dx=p.getX()-c.getX()
+            dy=p.getY()-c.getY()
+            newshape= shape.clone()
+            newshape.move(dx,dy)
+            newshape.draw(win)
+
+        t = Text(Point(100,100),"Click again to close")
+        t.draw(win)
+        win.getMouse()
+        win.close()
+main()
